14e96dcde333e2db74ffa96ac5e20a28f3200748
Crispin Anderson <37901056+crispinanderson@users.noreply.github.com>
2020-07-22 11:17:35 +0200

    prev === current applyProps / applyStates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Controller from './controller';
 const controller = new Controller();
 
 export const ExternalSVG = (props) => {
 
     const { applyStyles, applyProps, src, onError } = props;
+
     const [svg, setSVG] = useState(null);
     const [render, setRender] = useState(null);
 
-
+    //Fetch and set the svg
     useEffect(() => {
 
         if (src.includes('/') && src.includes('.svg')) {
@@ -31,8 +32,19 @@ export const ExternalSVG = (props) => {
 
     }, [src]);
 
+    //Object equality check prev & current props
+    const prevApplyProps = useRef(applyProps);
+    const prevApplyStyles = useRef(applyStyles);
+    const shouldRedraw = JSON.stringify(prevApplyProps.current) === JSON.stringify(applyProps) || JSON.stringify(prevApplyStyles.current) === JSON.stringify(applyStyles);
+
     useEffect(() => {
-        if (svg && svg.includes('<svg')) {
+        prevApplyProps.current = applyProps;
+        prevApplyStyles.current = applyStyles;
+    }, [applyProps, applyStyles])
+
+    //Create render
+    useEffect(() => {
+        if (svg && svg.includes('<svg') || shouldRedraw) {
             try {
                 const converted = controller.convert(svg, { applyProps, applyStyles });
                 setRender(converted)
@@ -46,7 +58,9 @@ export const ExternalSVG = (props) => {
                 }
             }
         }
-    }, [svg, applyProps, applyStyles])
+    }, [svg, applyProps, applyStyles]);
+
+
 
     return (<>{render}</>)
 }
