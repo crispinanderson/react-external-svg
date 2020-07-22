3f5b66229f585c0126b89edd21ce72f7cf8ead63
Crispin Anderson <37901056+crispinanderson@users.noreply.github.com>
2020-07-22 10:22:33 +0200

    webpack babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,46 @@
 
+const nodeExternals = require('webpack-node-externals');
+
+var path = require('path');
+
 module.exports = {
-    mode: "production",
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
+    entry: './src/index.js',
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    target: 'node',
+    externals: [nodeExternals()],
     module: {
         rules: [
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
-                enforce: "pre",
                 test: /\.js$/,
-                loader: "source-map-loader"
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: [
+                            '@babel/preset-env',
+                            {
+                                plugins: [
+                                    '@babel/plugin-transform-react-jsx'
+                                ]
+                            }
+                        ]
                     }
                 }
             }
         ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    },
+    resolve: {
+        alias: {
+            react: path.resolve('./node_modules/react')
+        }
     }
-
-
 };
